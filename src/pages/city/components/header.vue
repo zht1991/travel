49b2fe12540469087cecad94742f5c9b31393b97
zht <877070017@qq.com>
2018-10-10 21:39:10 +0800

    city router

---
+++ src/pages/city/components/header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+<style lang='stylus' scoped>
+  @import '~styles/varibles.styl'
+  .header
+    position relative
+    overflow hidden
+    height $headerHeight
+    line-height $headerHeight 
+    color #fff
+    text-align center
+    background $bgColor
+    font-size .32rem
+    .header-back
+      width 0.64rem
+      font-size .4rem
+      text-align center
+      position absolute
+      top 0
+      left 0
+      color #fff
+</style>
